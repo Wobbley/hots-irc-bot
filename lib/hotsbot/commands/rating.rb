530e4df04187679adda5798cce4498eac73f2a98
chadrien <chadrien@chadrien.fr>
2015-02-11 13:25:29 +0100

    Refactor: extract methods — refs #20

--- lib/hotsbot/commands/rating.rb
+++ lib/hotsbot/commands/rating.rb
@@ -16,18 +16,14 @@ module Hotsbot
       command :rating, { username: :string }, summary: 'Replies with a list of players with the given BattleTag from HotsLogs'
 
       def rating(m, username=nil, page=nil)
-        if username.nil?
-          m.target.send 'Get Hotslogs rating from BattleTag, example: !rating username'
-        else
-          tr_elements_with_mmr = get_tr_elements_with_mmr(page, username)
+        return m.target.send 'Get Hotslogs rating from BattleTag, example: !rating username' if username.nil?
 
-          if tr_elements_with_mmr.empty?
-            m.target.send 'No rating found for this username'
-          else
-            send_the_five_firsts_mmr(m, tr_elements_with_mmr)
-            m.user.send "Here's the full list of #{username} ratings: #{Rating::URL}#{username}" if tr_elements_with_mmr.count > 5
-          end
-        end
+        tr_elements_with_mmr = get_tr_elements_with_mmr(page, username)
+
+        return m.target.send 'No rating found for this username' if tr_elements_with_mmr.empty?
+
+        send_the_five_firsts_mmr(m, tr_elements_with_mmr)
+        m.user.send "Here's the full list of #{username} ratings: #{Rating::URL}#{username}" if tr_elements_with_mmr.count > 5
       end
 
       def send_the_five_firsts_mmr(m, tr_elements_with_mmr)
@@ -54,8 +50,13 @@ module Hotsbot
         end
 
         page.css('tbody tr').to_a.select do |tr|
-          not tr.nil? and not tr.css('td')[4].nil? and %r{\d+}.match(tr.css('td')[4].text)
+          rating_td = tr.css('td')[4]
+          td_has_rating(rating_td, tr)
         end
+      end
+
+      def td_has_rating(rating_td, tr)
+        not tr.nil? and not rating_td.nil? and %r{\d+}.match(rating_td.text)
       end
     end
   end
