9147452d036501b81fbf00f4d512a3204e26bc82
chadrien <chadrien@chadrien.fr>
2015-02-14 16:39:27 +0100

    Fix missing command description for url_printer

--- lib/hotsbot/commands/url_printer.rb
+++ lib/hotsbot/commands/url_printer.rb
@@ -2,7 +2,7 @@ module Hotsbot
   module Commands
     module UrlPrinter
       def self.included(klass)
-        klass.class_eval "command :#{klass::COMMAND}"
+        klass.class_eval "command :#{klass::COMMAND}, {}, summary: \"#{klass::COMMAND_SUMMARY}\", description: \"#{klass::COMMAND_SUMMARY}\""
         define_method klass::COMMAND do |m|
           m.target.send klass::COMMAND_MESSAGE
         end
