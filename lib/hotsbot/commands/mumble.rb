c97e430b7c089c95687bb1b63f79523d5fe0f62b
chadrien <chadrien@chadrien.fr>
2015-02-22 13:35:28 +0100

    Set correct mumble URL — fixes #32

--- lib/hotsbot/commands/mumble.rb
+++ lib/hotsbot/commands/mumble.rb
@@ -12,7 +12,7 @@ module Hotsbot
 
       COMMAND = 'mumble'
       COMMAND_SUMMARY = 'Prints server info of the Reddit Mumble'
-      COMMAND_MESSAGE = 'The Reddit community mumble server can be found here: ts3.oda-h.com'
+      COMMAND_MESSAGE = 'The Reddit community mumble server can be found here: hotsreddit.no-ip.org ; port: 7000'
       include UrlPrinter
     end
   end
